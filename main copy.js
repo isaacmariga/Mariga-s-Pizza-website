f05548437b85971fb88181ad1322ead63938501b
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-25 19:05:00 +0300

    recording previous orders

--- main copy.js
+++ main copy.js
@@ -8,6 +8,7 @@ $('.top').mousedown(function(e) {
     $(this).prop('selected', !$(this).prop('selected'));
     return false;
 });
+
 $("form").click(function() {
     size = $('#size').val();
     crust = $('#crust').val();
@@ -15,13 +16,13 @@ $("form").click(function() {
 
     switch (size) {
         case "large":
-            size = 700;
+            sizePrice = 700;
             break;
         case "medium":
-            size = 500;
+            sizePrice = 500;
             break;
         case "small":
-            size = 300;
+            sizePrice = 300;
             break;
         default:
             console.log("Error");
@@ -72,93 +73,43 @@ $("form").click(function() {
         delivery = 0
     }
 
-    totalBill = size + crustPrice + toppingTotal + delivery
+    totalBill = sizePrice + crustPrice + toppingTotal + delivery
     console.log(totalBill)
+
+    pizza.prototype.Order = function() {
+        return this.size + " " + this.crust + " " + this.topping;
+    }
+
+
 })
+$("form#order").submit(function(event) {
+    event.preventDefault();
+
+    let newOrder = new pizza(size, crust, topping);
+
+
+
+    $("ul#contacts").append("<li><span class='contacts'>" + newOrder.Order() + "</span></li>");
+
+    $("select#size").val("");
+    $("select#crust").val("");
+    $("input#delivery").val("");
+
+    $(".contact").last().click(function() {
+        $("#show-order").show();
+        $("#show-order h2").text(newOrder.size);
+        $(".order_size").text(newOrder.size);
+        $(".order_crust").text(newOrder.crust);
+        $(".order_topping").text(newOrder.topping);
+
+        console.log(newOrder)
+    });
+});
 
 
 
 
 
-
-
-
-
-
-
-// $('option').on('click', () => {
-//     let crust = $('#crust').val();
-
-
-
-//     let crust = $('#crust').val();
-//     let topping = $('.top').val();
-
-
-
-// })
-
-
-// $('.top').mousedown(function(e) {
-//     e.preventDefault();
-//     $(this).prop('selected', !$(this).prop('selected'));
-//     return false;
-// });
-// $('.radios').on('click', () => {
-//     if ($('#delivery').is(":checked")) {
-//         delivery = 200;
-//     } else {
-//         delivery = 0
-//     }
-//     console.log(delivery)
-// })
-
-
-
-
-
-
-
-// let pizza1 = new pizza(size, crust, topping)
-
-
-
-// // console.log(pizza1)
-
-
-// let toppingOptions = ["ham", "peperoni", "mushroom", "steak", "pickles"]
-// let toppingPrices = [50, 70, 40, 20, 20]
-// let toppingAmount = [];
-
-
-// let findPrices = (first, second) => {
-//     first.forEach((element, index) => {
-//         if (second.includes(element)) {
-//             toppingAmount.push(toppingPrices[index]);
-//         };
-//     });
-//     return toppingAmount;
-// };
-// findPrices(toppingOptions, topping);
-
-// let toppingTotal = toppingAmount.reduce((a, b) => a + b, 0)
-// console.log(toppingTotal)
-
-
-
-
-// var sizePrice = basePrice
-// var deliveryFee = delivery
-
-
-
-// let orderTotal = deliveryFee + basePrice + crustPrice + toppingTotal
-
-
-
-// console.log(orderTotal)
-
-// console.log(sizePrice)
 
 // $("form#order").submit(function(event) {
 //     event.preventDefault();
