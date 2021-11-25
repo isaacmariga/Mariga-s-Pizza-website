106af8887f121e0c973027183aa7946c7b0d7b80
isaacmariga <isaac.wang'ombe@student.moringaschool.com>
2021-11-25 06:38:47 +0300

    setting up switch statements for pizza size

--- main.js
+++ main.js
@@ -2,8 +2,27 @@ function pizza(size, crust, topping) {
     this.size = size;
     this.crust = crust;
     this.topping = topping;
+
+
+
+
 }
 
 let pizza1 = new pizza("large", "crispy", ["peperoni", "cheese"])
 
-console.log(pizza1)
+
+switch (pizza1.size) {
+    case "large":
+        basePrice = 700;
+        break;
+    case "medium":
+        basePrice = 500;
+        break;
+    case "small":
+        basePrice = 300;
+    default:
+        alert("Error")
+
+}
+
+console.log(basePrice)
